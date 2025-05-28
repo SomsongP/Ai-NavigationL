7204f59997758c1085b5add2f06383f1b895f14d
Thanakrit66070262 <thanakritktsn@gmail.com>
2025-05-28 16:56:56 +0700

    Part 2-3+random path func

--- Assets/AgentManager.cs
+++ Assets/AgentManager.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using UnityEngine.UIElements;
-
+using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Collections;
 public class AgentManager : MonoBehaviour
 {
-    GameObject [] agents;
+
+    List<NavMeshAgent> agents = new List<NavMeshAgent>();
+    IEnumerator coroutine;
     void Start()
     {
-        agents = GameObject.FindGameObjectsWithTag("AI");
+        
+        GameObject[] a = GameObject.FindGameObjectsWithTag("AI");
+        foreach(GameObject go in a)
+        {
+            agents.Add(go.GetComponent<NavMeshAgent>());
+        }
     }
 
     // Update is called once per frame
@@ -16,11 +25,30 @@ public class AgentManager : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
-                foreach (GameObject a in agents)
+                foreach (NavMeshAgent a in agents)
                 {
-                    a.GetComponent<AiController>().agent.SetDestination(hit.point);
+                    a.SetDestination(hit.point);
                 }
             }
         }
+        coroutine = RanWalk(20f);
+        StartCoroutine(coroutine);
+        
     }
+    private IEnumerator RanWalk(float time)
+    {
+        Vector3 randomPosition = new Vector3(
+    Random.Range(-100f, 100f),
+    0f,
+    Random.Range(-100f, 100f));
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            foreach (NavMeshAgent a in agents)
+            {
+                a.SetDestination(randomPosition);
+                Debug.Log("RUnto" + randomPosition);
+            }
+        }
+     }
 }
--- Assets/AiController.cs
+++ Assets/AiController.cs
@@ -4,14 +4,23 @@ using UnityEngine.AI;
 public class AiController : MonoBehaviour
 {
     public NavMeshAgent agent;
+    Animator anim;
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance < 1)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetBool("isMoving", true);
+        }
     }
 }
